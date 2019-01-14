c0d8649ba262cced6e022f9474f1e6a82c2f75c5
江恒 <1139209675@qq.com>
2019-01-14 21:02:00 +0800

    rewrite

---
+++ src/Exceptions/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * NotFoundException
+ *
+ * User: jiangheng
+ * Date: 2019/1/14
+ * Time: 17:55
+ */
+
+namespace Jackdou\Swoole\Exceptions;
+
+class NotFoundException extends \Exception
+{
+
+}
